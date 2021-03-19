0b8646336ab9e9b1f5f185057020fa9281045127
Chad Burkins <cburkin@its.jnj.com>
2021-03-19 16:45:16 -0400

    Comments explaining empty methods inside Context definition

--- oidc-react-app/src/providers/authProvider.jsx
+++ oidc-react-app/src/providers/authProvider.jsx
@@ -3,6 +3,14 @@
 import React, { Component } from "react";
 import AuthService from "../services/authService";
 
+// 1st and only argument to .createContext() is defaultValue
+// defaultValue is *only* used when a component does not have a matching Provider above it in tree
+// Docs say perhaps useful of testing components in isolation with wrapping them
+//
+// This article says it's useful for destructuring the associated methods within the consumer
+// More info here: https://kentcdodds.com/blog/how-to-use-react-context-effectively
+// Tested, and destructing within the Consumer still works without the defaultValue object
+// So could easily delete 1st (only only) argument to .createContext()
 const AuthContext = React.createContext({
     signinRedirectCallback: () => ({}),
     logout: () => ({}),
@@ -22,6 +30,8 @@ export class AuthContextProvider extends Component {
         this.authService = new AuthService();
     }
     render() {
+        // "value" prop will passed to all Consumers of this Context
+        // In this case, Consumers will be given access to oidc-client.js via authService
         return <AuthContext.Provider value={this.authService}>{this.props.children}</AuthContext.Provider>;
     }
 }
