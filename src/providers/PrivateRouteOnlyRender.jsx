f482affc9e75e702258d0a4eed69dbbcecb83c99
Chad Burkins <cburkin@its.jnj.com>
2021-04-11 13:19:19 -0400

    Little bit more readable

--- src/providers/PrivateRouteOnlyRender.jsx
+++ src/providers/PrivateRouteOnlyRender.jsx
@@ -2,9 +2,20 @@ import React, { useContext } from "react";
 import { Route, Redirect } from "react-router-dom";
 import { AuthContext } from "./authProvider";
 
-export const PrivateRouteOnlyRender = ({ component: Component, ...rest }) => {
+export const PrivateRouteOnlyRender = ({ render: orig_render, ...rest }) => {
+    //
     // Get "context" which gives us access to the getUser() function later
     let authContext = useContext(AuthContext);
 
-    return <Route {...rest} render={(props) => (authContext.isAuthenticated() ? rest.render(props) : <Redirect to="/login" />)} />;
+    return (
+        <Route
+            // Pass through rest of props originally used
+            {...rest}
+            // And the render function
+            render={(props) =>
+                // If user is authenticaed, render desired function
+                authContext.isAuthenticated() ? orig_render(props) : <Redirect to="/login" />
+            }
+        />
+    );
 };
